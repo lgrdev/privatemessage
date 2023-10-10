ffcf8147adcf2d56f17c940463fc4931c700eeac
lgrdev <l.glesner@gmail.com>
2023-10-10 19:27:14 +0000

    memcache storage

--- config/config.env.php
+++ config/config.env.php
@@ -8,6 +8,10 @@ return [
     // 'db.user'       => DI\env('db_user', 'myuser'),
     // 'db.password'   => DI\env('db_password', 'mypassword'),
 
+    // uncomment the folowing lines if you use Memcache
+    // 'mc.host'    => DI\env('mc_host', 'localhost'),
+    // 'mc.port'    => DI\env('mc_port', 11211),
+
     // if you use Redis
     'redis.host'    => DI\env('redis_host', 'localhost'),
     'redis.port'    => DI\env('redis_port', 6379),
